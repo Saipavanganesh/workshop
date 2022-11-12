96768b88389dbf82c8bfb6fb0ec05ca1d8629038
Saipavanganesh <pavanganeshms@gmail.com>
2022-11-12 19:55:20 +0530

    Second usecase

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,2 +1,24 @@
-flip=$(($RANDOM%2));
+heads=0;
+tails=0;
 
+declare -a arr_heads;
+declare -a arr_tails;
+
+for ((i=0;i<=20;i++))
+do
+	flip=$(($RANDOM%2));
+	if [ $flip -eq 0 ]
+	then
+		arr_heads[$heads]="Heads";
+		heads=$(($heads+1));
+		
+	else
+		arr_tails[$tails]="Tails";
+		tails=$(($tails+1));
+	fi
+done
+
+echo ${arr_heads[@]};
+echo ${arr_tails[@]};
+
+echo "Count of heads is $heads and count of tails is $tails";
